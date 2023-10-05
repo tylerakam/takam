c5fde8a420c9af8b7e6ff04eedfee1c5715bbd27
TylerAkam <tza13@psu.edu>
2023-10-05 12:59:17 -0400

    added javascript testing

---
+++ docs/javaTesting.js
@@ -0,0 +1,18 @@
+window.onload = alert ('Enter if you dare!');
+
+function ad() {
+  document.getElementById("sub").innerHTML = "Subscribe! (oops, the link is gone. go to Chiefofbricks!)";
+}
+function bigImg(x) {
+  x.style.height = "439.5px";
+  x.style.width = "1212px";
+}
+
+function normalImg(x) {
+  x.style.height = "293px";
+  x.style.width = "808px";
+}
+
+//For this I tried a few things and made some adjustments to fit my code. I think what I did was kind of basic, but I want to keep experimenting and figure out what I can do. I think that's a limitation at the moment, as I don't know the full capabilities of JS''//
+
+// Akam-JS1-2023-10-05
